e63690eec7707a01dd1403e36c6923225744f2d4
Brany Suero <branysuero@gmail.com>
2018-08-25 22:06:25 -0400

    Stack

---
+++ Java/DS/Stack/Stack.java
@@ -0,0 +1,33 @@
+public class Stack {
+
+    //LIFO
+
+    private int maxSize;                                //size of the stack
+    private long[] stackArray;                          //container to store the list of items
+    private int top;                                    //index of the last item
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new long[maxSize];
+        this.top = -1;
+    }
+
+    public void push(long j) {
+        
+        top++;
+        stackArray[top] = j;
+    }
+
+    public long pop() {
+        
+        int old_top = top;
+        top--;
+        return stackArray[old_top];
+    }
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
